785b2b254d65aab01e8d9a2bc71f3ef2be777927
Chris Hafey <chafey@gmail.com>
2020-04-30 11:16:27 -0500

    .

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ sudo apt-get install curl
 # Get various dot files
 curl -L https://raw.githubusercontent.com/chafey/dot-files/master/setup-dot-files.sh | bash
 
+# bin folder
+curl -L https://raw.githubusercontent.com/chafey/dot-files/master/setup-bin.sh | bash
+
 # Dev Tools
 curl -L https://raw.githubusercontent.com/chafey/dot-files/master/setup-dev.sh | bash
 
